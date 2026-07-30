f581a447ec89bdb222328350108428f018b76880
uday1597 <udaysurya1597@gmail.com>
2026-07-30 10:17:33 +0530

    cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,18 +22,21 @@ app = FastAPI(
     version="0.1.0"
 )
 
-origins = [
-    "http://localhost:3000",
-]
+# origins = [
+#     "http://localhost:3000",
+# ]
 
-frontend_url = os.getenv("FRONTEND_URL")
+# frontend_url = os.getenv("FRONTEND_URL")
 
-if frontend_url:
-    origins.append(frontend_url)
+# if frontend_url:
+#     origins.append(frontend_url)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
